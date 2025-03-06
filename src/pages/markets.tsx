47b0db8e7119fdf1a7c1f49e7fa59e31f5113656
lmohade3l <mohade3.2710@gmail.com>
2025-03-07 00:50:09 +0330

    feat: add swr and make the api call for makets

--- src/pages/markets.tsx
+++ src/pages/markets.tsx
@@ -1,4 +1,11 @@
+import useSWR from "swr"
+
 export default function Markets() {
+
+    const fetcher = (...args: [string, RequestInit?]) => fetch(...args).then(res => res.json())
+    const { data, error } = useSWR("https://api.bitpin.org/v1/mkt/markets/", fetcher)
+
+
     return (
         <div>markets page</div>
     )
